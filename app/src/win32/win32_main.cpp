a4bf4112ca3a46dd23d1081d3662126735b4bb54
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2022-05-10 17:56:11 +0300

    Implement custom messaging loop

--- app/src/win32/win32_main.cpp
+++ app/src/win32/win32_main.cpp
@@ -1,17 +1,90 @@
-#include "win32_min.h"
+#include "win32_win.h"
 
 #include <engine/test.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+#include <iostream>
+
+static void InitConsole();
+static HWND InitWindow(HINSTANCE hInstance);
+static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nShowCmd
+)
 {
+    InitConsole();
+    Engine::Test("Test");
+
+    HWND hwnd = InitWindow(hInstance);
+    ShowWindow(hwnd, nShowCmd);
+
+    MSG msg;
+    while(true)
+    {
+        bool isQuitPosted = false;
+        while(PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
+                isQuitPosted = true;
+            }
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        if(isQuitPosted)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
+
+void InitConsole()
+{    
     AllocConsole();
     FILE* dummy;
     auto s = freopen_s(&dummy, "CONOUT$", "w", stdout);
+}
 
-    Engine::Test("Test");
+HWND InitWindow(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wc{};
+    wc.cbSize = sizeof(wc);
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"RendererWindow";
 
-    while (true)
+    RegisterClassExW(&wc);
+
+    HWND hwnd = CreateWindowExW(NULL,
+                                L"RendererWindow",
+                                L"Test",
+                                WS_OVERLAPPEDWINDOW,
+                                CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720,
+                                NULL,
+                                NULL,
+                                hInstance,
+                                NULL);
+
+    return hwnd;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
     {
-        
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            return 0;
+        } break;
     }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
 }
